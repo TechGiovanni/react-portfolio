e4b0266dc8372fcfcdb394d3e69c1427605dea1f
Giovanni Reid <giovanni-mtl@hotmail.com>
2023-01-18 15:59:25 -0500

    customizing webapp

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,37 +1,54 @@
 import { useEffect } from 'react'
-import HeaderImage from '../../assets/header.jpg'
+import HeaderImage from '../../assets/header-Hero-img.jpeg'
 import data from './data'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import './header.css'
 
 const Header = () => {
-  useEffect(() => {
-    AOS.init({duration: 2000})
-  }, [])
+	useEffect(() => {
+		AOS.init({ duration: 2000 })
+	}, [])
 
-  return (
-    <header id="header">
-      <div className="container header__container">
-        <div className="header__profile" data-aos="fade-in">
-          <img src={HeaderImage} alt="Header Portait" />
-        </div>
-        <h3  data-aos="fade-up">Hajia Bintu</h3>
-        <p  data-aos="fade-up">
-        You are a click away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!
-        </p>
-        <div className="header__cta"  data-aos="fade-up">
-          <a href="#contact" className='btn primary'>Let's Talk</a>
-          <a href="#portfolio" className='btn light'>My Work</a>
-        </div>
-        <div className="header__socials">
-          {
-            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-          }
-        </div>
-      </div>
-    </header>
-  )
+	return (
+		<header id='header'>
+			<div className='container header__container'>
+				<div className='header__profile' data-aos='fade-in'>
+					<img src={HeaderImage} alt='Header Portait' />
+				</div>
+				<h3 data-aos='fade-up'>Giovanni Reid</h3>
+				<p data-aos='fade-up'>
+					I’m Giovanni. I’m a Montreal-based frontend engineer passing one year
+					of experience, currently focused on full-stack development and user
+					experience. In my previous careers, I have led teams and worked with a
+					lot of great people. Now I have evolved and grown my skills to being
+					more tech focused by networking with various individuals in the tech
+					industry, and to working in teams to build and deliver great
+					applications.
+				</p>
+				<div className='header__cta' data-aos='fade-up'>
+					<a href='#contact' className='btn primary'>
+						Let's Talk
+					</a>
+					<a href='#portfolio' className='btn light'>
+						My Work
+					</a>
+				</div>
+				<div className='header__socials'>
+					{data.map((item) => (
+						<a
+							key={item.id}
+							href={item.link}
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							{item.icon}
+						</a>
+					))}
+				</div>
+			</div>
+		</header>
+	)
 }
 
 export default Header
--- src/sections/testimonials/data.js
+++ src/sections/testimonials/data.js
@@ -1,64 +1,51 @@
-import Avatar1 from '../../assets/avatar1.jpg'
-import Avatar2 from '../../assets/avatar2.jpg'
-import Avatar3 from '../../assets/avatar3.jpg'
-import Avatar4 from '../../assets/avatar4.jpg'
-import Avatar5 from '../../assets/avatar5.jpg'
-import Avatar6 from '../../assets/avatar6.jpg'
-import Avatar7 from '../../assets/avatar7.jpg'
-
+import Avatar1 from '../../assets/testimonial-Lisandra.png'
+import Avatar2 from '../../assets/testimonial-Melisa.png'
+import Avatar3 from '../../assets/testimonial-Etienne.png'
+import Avatar4 from '../../assets/testimonial-Jenny.png'
+import Avatar5 from '../../assets/testimonial-Arstanbek.png'
 
 const data = [
-        {
-        id: 1,
-        quote: "It's truly amazing how you did exactly what I want with the little information I gave. You'll be my go-to developer from today. Highly recommend!",
-        avatar: Avatar1,
-        name: 'Nana Ama McBrown',
-        profession: 'Actress'
-        },
-        {
-        id: 2,
-        quote: "Good job, Hajia! Right on time! I really like how the project turned out, and my customers like it as well. It was fun collaborating with you.",
-        avatar: Avatar2,
-        name: 'Ernest Achiever',
-        profession: 'Web Developer'
-        },
-        {
-        id: 3,
-        quote: "Impressive work, buddy. Highly recommend!",
-        avatar: Avatar3,
-        name: 'Daniel Vinyo',
-        profession: 'Graphic Designer'
-        },
-        {
-        id: 4,
-        quote: "Wow. You finished earlier than you said. Everything's modern and beautifully done to the T. Great profession!",
-        avatar: Avatar4,
-        name: 'Diana Ayi',
-        profession: 'Singer'
-        },
-        {
-        id: 5,
-        quote: "I like how the app turned out. You are incredible, Hajia.",
-        avatar: Avatar5,
-        name: 'Vivian Mensah',
-        profession: 'Street Photographer'
-        },
-        {
-        id: 6,
-        quote: "Best developer in the world!",
-        avatar: Avatar6,
-        name: 'Hajia Bintu',
-        profession: 'Brand Influencer'
-        },
-        {
-        id: 7,
-        quote: "Keep doing great stuff, dear! Nicely executed!",
-        avatar: Avatar7,
-        name: 'Nicki Nyarko',
-        profession: 'Makeup Artist'
-        }
-    ]
+	{
+		id: 1,
+		quote: `I have worked with Giovanni in Le Wagon and it has been a pleasure. He's very friendly and always willing to lend a helping hand.
+        He also has a very solid understanding of front-end and I've seen his projects where his skills translated to great end results.
+        I have no doubt that he would be an asset to any team or organization :)`,
+		avatar: Avatar1,
+		name: 'Liandra Bautista',
+		profession: 'Full Stack Developer',
+	},
+	{
+		id: 2,
+		quote:
+			"Giovanni's enthusiasm and curiosity for web development is admirable and contagious. I had the pleasure of working alongside someone who is driven to learn and who contributes creative solutions to solving problems. I’m grateful for our exchanges on the latest technologies and programming resources. Giovanni will be a great asset in any team and I wish him the best in all his endeavors.",
+		avatar: Avatar2,
+		name: 'Melissa BG',
+		profession: 'Web Developer',
+	},
+	{
+		id: 3,
+		quote:
+			'I enjoyed getting to know Giovanni during our time together at Le Wagon. I was impressed with his skills as a coder and his creative prowess for front end design. His determination to find solutions to error messages and issues using all available resources was commendable. Giovanni is great talent and a fine asset to any team.',
+		avatar: Avatar3,
+		name: 'Etienne A Forcet',
+		profession: 'Web Developer',
+	},
+	{
+		id: 4,
+		quote:
+			'Giovanni has demonstrated impressive front-end skills during our projects at Le Wagon Montréal Bootcamp. A creative, passionate, skilled and hard-working developer, Giovanni is also a great team player and is an excellent addition to any team.',
+		avatar: Avatar4,
+		name: 'Jin (Jenny) Qi',
+		profession: 'Full Stack Web developer',
+	},
+	{
+		id: 5,
+		quote:
+			'I had the pleasure to study together with Giovanni at Le Wagon Montreal Bootcamp. Giovanni is highly proficient in Ruby programming language, Ruby on Rails, Javascript, HTML, CSS, Bootstrap. I highly recommend Giovanni for your projects which require Front-End work.',
+		avatar: Avatar5,
+		name: 'Arstanbek Zholochu',
+		profession: 'Full Stack Web developer',
+	},
+]
 
-
-
-    export default data
+export default data
